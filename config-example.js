6aeaefe0b6cb7318a3ca409c49b25e14f50425a0
Lauri Lavanti <lauri.lavanti@futurice.com>
2016-08-15 09:48:33 +0300

    Switch from JShint to ESlint. Receive a single notification from Gmail (need to make it constant until end)

--- config-example.js
+++ config-example.js
@@ -7,17 +7,24 @@ const config = {
   trello_list_id: 'The id of the list to publish new cards to',
   // Choose appropriate auth scopes from https://developers.google.com/gmail/api/auth/scopes
   // To receive e-mails follow this guide: https://developers.google.com/gmail/api/guides/push
-  gmail_scopes: [
+  g_scopes: [
     'https://www.googleapis.com/auth/gmail.send',
     'https://www.googleapis.com/auth/gmail.readonly'
   ],
   // Get google auth from https://console.developers.google.com/apis/library
   // https://github.com/google/google-api-nodejs-client/#authorizing-and-authenticating
-  gmail_client_id: 'Your Oauth2 client id',
-  gmail_client_secret: 'Your Oauth2 client secret',
-  gmail_redirect_url: '',
+  g_client_id: 'Your Oauth2 client id',
+  g_client_secret: 'Your Oauth2 client secret',
+  // If you have an actual site you wish to redirect to change the following value:
+  g_redirect_url: 'https://google.fi',
   // Set the topic in Google's pubsub developer console
+  // Add serviceAccount:gmail-api-push@system.gserviceaccount.com as a Pub/sub-publisher to the topic
   gmail_topic: 'projects/your-project/your-topic',
+  // These values are received when authenticating the application for the first time
+  g_access_token: '',
+  g_token_type: '',
+  g_refresh_token: '',
+  g_expiry_date: 12345678910,
   // gmail_reply_email is the text you want to send as a response to a new thread
   gmail_reply_email: 'Hi, this is an automated message from g2tbl, your e-mail has been added to Trello'
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+/* eslint no-undef: 'off', no-console: 'off' */
+const readline = require('readline');
 const Trello = require('node-trello');
 const google = require('googleapis');
 const config = require('./config');
 
 const t = new Trello(config.trello_key, config.trello_token);
 const gmail = google.gmail('v1');
+const OAuth2 = google.auth.OAuth2;
+const oauth2Client = new OAuth2(config.g_client_id, config.g_client_secret, config.g_redirect_url);
 
 function createCard(name, desc, callback) {
   const newCard = { name, desc, idList: config.trello_list_id };
@@ -13,12 +17,46 @@ function createCard(name, desc, callback) {
   });
 }
 
-const watchRequest = {
-  topicName: config.gmail_topic,
-  labelIds: ['INBOX']
+// Built on top of the Google example here:
+// https://github.com/google/google-api-nodejs-client/blob/master/samples/oauth2.js
+function getAccessToken() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: config.g_scopes
+  });
+  console.log('Visit this url:\n', url);
+  rl.question('Enter the code on the url (after ?code=) here:', function(code) {
+    oauth2Client.getToken(code, function(err, tokens) {
+      if (err) throw err;
+      console.log('Add the following tokens to config.js:\n', tokens);
+    });
+  });
 }
 
-// TODO:
-// https://www.sitepoint.com/mastering-your-inbox-with-gmail-javascript-api/
-// https://developers.google.com/gmail/api/guides/push
-// https://github.com/google/google-api-nodejs-client/#installation
+if (config.g_refresh_token === '' && config.g_access_token === '') {
+  getAccessToken();
+  process.exit();
+} else {
+  oauth2Client.setCredentials({
+    access_token: config.g_access_token,
+    token_type: config.g_token_type,
+    refresh_token: config.g_refresh_token,
+    expiry_date: config.g_expiry_date
+  });
+  const watchRequest = {
+    auth: oauth2Client,
+    userId: 'me',
+    resource: {
+      topicName: config.gmail_topic,
+      labelIds: ['INBOX']
+    }
+  };
+  gmail.users.watch(watchRequest, function(err, data) {
+    if (err) throw err;
+    console.log('Got a push notification:', data);
+  });
+}
